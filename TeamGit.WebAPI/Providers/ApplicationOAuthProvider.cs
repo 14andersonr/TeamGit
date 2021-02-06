cf0e57abe5b49f21e636d2e74e16e7364a04b7f2
KeithAnderson1345 <KeithAnderson1345@gmail.com>
2021-02-06 11:02:08 -0500

    Created and tested Post method

--- TeamGit.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ TeamGit.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
+using TeamGit.Data;
 using TeamGit.WebAPI.Models;
 
 namespace TeamGit.WebAPI.Providers
